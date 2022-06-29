8a08bc1f9b0c350ef02f950129a4714d2c9fc2e5
erico07181 <erico@salesforce.com>
2022-06-28 17:00:50 -0700

    step 2 done

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,32 @@
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Home from "../Home/Home";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TransactionDetail from "../TransactionDetail/TransactionDetail";
+import { useState } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState();
+  const [transactions, setTransactions] = useState();
+  const [transfers, setTransfers] = useState();
+  const [error, setError] = useState();
+  const [filterInputValue, setFilterInputValue] = useState("");
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
+    <div className="app">
+      <BrowserRouter>
+        <Navbar
+          filterInputValue={filterInputValue}
+          setFilterInputValue={setFilterInputValue}
+        >
+          <div className="main">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </Navbar>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
